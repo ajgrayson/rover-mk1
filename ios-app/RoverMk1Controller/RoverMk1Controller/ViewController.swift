0472fcfc07f4113e37b3a322b9d415eb1e3cbba5
John Grayson <johnathangrayson@Johnathans-MacBook-Pro-3.local>
2014-12-10 21:32:16 +1300

    Initial commit
    Setup basic arduino though no running code
    Setup initial iOS app

---
+++ ios-app/RoverMk1Controller/RoverMk1Controller/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RoverMk1Controller
+//
+//  Created by Johnathan Grayson on 10/12/14.
+//  Copyright (c) 2014 Johnathan Grayson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
